ed1608d315caf45c654b2b64499dd3522b31366b
LuanRaabe <luan.raabe202@gmail.com>
2021-12-16 09:08:01 -0300

    Calculadora de juros 3.0

--- 3-JavaScript/JurosV2/scripts.js
+++ 3-JavaScript/JurosV2/scripts.js
@@ -8,6 +8,67 @@ var debts = {
 
 var clients = [];
 
+function addSomeClients() {
+  clients.push(
+    {
+      name: 'Fulano da Silva',
+      date: '2015-08-20',
+      valorOriginal: 500,
+      days: 404,
+      valueToPay: 504
+    }, {
+    name: 'Cicrano Costa',
+    date: '2016-07-12',
+    valorOriginal: 500,
+    days: 404,
+    valueToPay: 504
+  }, {
+    name: 'Beltrano Fonseca',
+    date: '2017-09-02',
+    valorOriginal: 500,
+    days: 404,
+    valueToPay: 504
+  }, {
+    name: 'Beltrano Fonseca',
+    date: '2014-06-15',
+    valorOriginal: 500,
+    days: 404,
+    valueToPay: 504
+  }, {
+    name: 'Fulano da Silva',
+    date: '2018-10-27',
+    valorOriginal: 500,
+    days: 404,
+    valueToPay: 504
+  }, {
+    name: 'Cicrano Costa',
+    date: '2013-05-01',
+    valorOriginal: 500,
+    days: 404,
+    valueToPay: 504
+  }, {
+    name: 'Fulano da Silva',
+    date: '2019-11-16',
+    valorOriginal: 500,
+    days: 404,
+    valueToPay: 504
+  }, {
+    name: 'Cicrano Costa',
+    date: '2016-07-12',
+    valorOriginal: 500,
+    days: 404,
+    valueToPay: 504
+  }, {
+    name: 'Fulano da Silva',
+    date: '2019-11-16',
+    valorOriginal: 500,
+    days: 404,
+    valueToPay: 504
+  })
+
+  clients.forEach(printClients);
+}
+
 function printTotal() {
   let debtsOriginal = clients.reduce(function (acc, elm) {
     return acc += elm.valorOriginal;
@@ -22,6 +83,16 @@ function printTotal() {
 
   totalOriginal.innerText = debtsOriginal;
   totalWDebts.innerText = debtsWTime;
+}
+
+function printClients(debts, index) {
+  let tbody = document.getElementById("dataClients");
+  let div = document.createElement('div');
+  div.innerHTML = `<div>${debts.name}</div><div>${debts.date}</div><div>${debts.valorOriginal}</div><div>${debts.days}</div><div>${debts.valueToPay}</div>`;
+  div.setAttribute('class', 'line');
+  tbody.appendChild(div);
+
+  printTotal();
 }
 
 function addInfo() {
@@ -41,7 +112,7 @@ function addInfo() {
   debts.valorOriginal = Number(valor.value);
   clients.push(debts);
 
-  document.getElementById('form').reset();
+  document.getElementById('dataForm').reset();
 
   let tbody = document.getElementById("dataClients");
   let div = document.createElement('div');
@@ -78,8 +149,8 @@ function calcDebt() {
   printTotal();
 }
 
-function dropBox() {
-  let box = document.getElementById("instructions");
+function dropBox(string) {
+  let box = document.getElementById(string);
   if (box.style.display == "none") {
     box.style.display = "flex";
     box.style.flexDirection = "column";
@@ -112,4 +183,44 @@ function groupClient() {
 function groupDate() {
   clients.sort(compDate);
   calcDebt();
+}
+
+function totalPerClient() {
+  groupClient()
+  let i = 0,
+    current = 0,
+    last = 0,
+    tClients = [],
+    obj = [];
+  while (current < clients.length) {
+    for (i = current; i < clients.length; i++) {
+      if (clients[current].name == clients[i].name) {
+        last++;
+      }
+    }
+    obj = [];
+    for (i = current; i < last; i++) {
+      obj.push(clients[i]);
+    }
+    tClients.push(obj);
+    current = last;
+  }
+  console.log(tClients);
+  let total = document.getElementById("showTotalPerClient");
+  total.innerText = '';
+  tClients.forEach((item, index) => {
+    //Valor original por cliente 
+    let tOriginal = item.reduce(function (acc, elm) {
+      return acc += elm.valorOriginal;
+    }, 0);
+    //Valor com imposto por cliente
+    let tToPay = item.reduce(function (acc, elm) {
+      return acc += elm.valueToPay;
+    }, 0);
+    let total = document.getElementById("showTotalPerClient");
+    let div = document.createElement('div');
+    div.innerHTML = `<div>${item[index].name}</div><div>Total original</div><div>${tOriginal}</div><div>Total com juros</div><div>${tToPay}</div>`;
+    div.setAttribute('class', 'line');
+    total.appendChild(div);
+  });
 }
